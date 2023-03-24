2dfac873766506c5029287170f5dff5efdf32b8a
Loic Rossi <loic_2610@hotmail.fr>
2023-03-24 17:36:16 +0100

    Move player though map on keydown

--- index.js
+++ index.js
@@ -11,14 +11,62 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const image = new Image()
 image.src = './images/Ribelle_Town.png'
 
-image.onload = () => {
-  c.drawImage(image, -65, -650)
-}
-
 const playerImage = new Image()
 playerImage.src = './images/playerDown.png'
 
-playerImage.onload = () => {
+class Sprite {
+  constructor({
+    position,
+    velocity,
+    image
+  }) {
+    this.position = position
+    this.image = image
+  }
+
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y)
+  }
+}
+
+const background = new Sprite({
+  position: {
+    x: -65,
+    y: -650
+  },
+  image: image
+})
+
+const keys = {
+  z: {
+    pressed: false
+  },
+  q: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  arrowUp: {
+    pressed: false
+  },
+  arrowDown: {
+    pressed: false
+  },
+  arrowLeft: {
+    pressed: false
+  },
+  arrowRight: {
+    pressed: false
+  }
+}
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  background.draw()
   c.drawImage(playerImage,
     0, //?
     0, //?
@@ -29,4 +77,86 @@ playerImage.onload = () => {
     playerImage.width / 4, //*
     playerImage.height, //* actual position
   )
+
+  if (keys.z.pressed || keys.arrowUp.pressed) {
+    background.position.y += 3
+  }
+  if (keys.s.pressed || keys.arrowDown.pressed) {
+    background.position.y -= 3
+  }
+  if (keys.q.pressed || keys.arrowLeft.pressed) {
+    background.position.x += 3
+  }
+  if (keys.d.pressed || keys.arrowRight.pressed) {
+    background.position.x -= 3
+  }
 }
+animate()
+
+let lastKey = ''
+
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'z':
+      keys.z.pressed = true
+      lastKey = 'z'
+      break;
+    case 'q':
+      keys.q.pressed = true
+      lastKey = 'q'
+      break
+    case 's':
+      keys.s.pressed = true
+      lastKey = 's'
+      break
+    case 'd':
+      keys.d.pressed = true
+      lastKey = 'd'
+      break
+    case 'ArrowUp':
+      keys.arrowUp.pressed = true
+      lastKey = 'ArrowUp'
+      break
+    case 'ArrowDown':
+      keys.arrowDown.pressed = true
+      lastKey = 'ArrowDown'
+      break
+    case 'ArrowLeft':
+      keys.arrowLeft.pressed = true
+      lastKey = 'ArrowLeft'
+      break
+    case 'ArrowRight':
+      keys.arrowRight.pressed = true
+      lastKey = 'ArrowRight'
+      break
+  }
+})
+
+window.addEventListener('keyup', (e) => {
+  switch (e.key) {
+    case 'z':
+      keys.z.pressed = false
+      break;
+    case 'q':
+      keys.q.pressed = false
+      break
+    case 's':
+      keys.s.pressed = false
+      break
+    case 'd':
+      keys.d.pressed = false
+      break
+    case 'ArrowUp':
+      keys.arrowUp.pressed = false
+      break
+    case 'ArrowDown':
+      keys.arrowDown.pressed = false
+      break
+    case 'ArrowLeft':
+      keys.arrowLeft.pressed = false
+      break
+    case 'ArrowRight':
+      keys.arrowRight.pressed = false
+      break
+  }
+})
